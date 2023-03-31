7a156a6128cf387b827464b45a9bf99a234c3ab8
Janick Kaltenmark <janick.kaltenmark@swr.de>
2023-03-31 11:09:18 +0200

    Initial commit

---
+++ src/main/java/com/kaldev/vereinsmanager/VereinsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kaldev.vereinsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VereinsmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VereinsmanagerApplication.class, args);
+    }
+
+}
